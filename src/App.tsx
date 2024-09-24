f00090ae99b2760f10bf8c0a7209c88e72bdfb89
nvbinhsoft <nvbinhsoft@gmail.com>
2024-09-24 18:08:17 +0700

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import './App.css'
 import {BooksProvider} from "./BooksContext.tsx";
 import Books from "./components/Books.tsx";
+import {Route, BrowserRouter as Router} from "react-router-dom";
 
 function App() {
-
-  return (
-      <BooksProvider>
-          <div>
-              <Books>
-
-              </Books>
-          </div>
-      </BooksProvider>
-
-  )
+    return (
+        <BooksProvider>
+            <Router>
+                <Route path={"/"} element={<Books/>}></Route>
+                <h1>App</h1>
+                {/*<Route path={"/book"} element={<Navigate to={"/"}/>}></Route>*/}
+            </Router>
+        </BooksProvider>
+    )
 }
 
 export default App
