b27d3c6c1c0d9ed9009223f2804b06ed09600c9b
Binh Nguyen Van <nvbinhsoft@gmail.com>
2024-09-25 00:46:39 +0700

    feat: create secret component

---
+++ src/components/SecretBook.tsx
@@ -0,0 +1,12 @@
+
+export default function SecretBook() {
+    return (
+        <div>
+            <ul>
+                <li>Book 1</li>
+                <li>Book 2</li>
+                <li>Book 3</li>
+            </ul>
+        </div>
+    );
+}
