ba9037153c4a3324cce07d87390bfca77c6d6bfd
Binh Nguyen Van <nvbinhsoft@gmail.com>
2024-09-25 00:47:07 +0700

    feat: update book component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import './App.css'
 import {BooksProvider} from "./BooksContext.tsx";
 import Books from "./components/Books.tsx";
-import {Route, BrowserRouter as Router} from "react-router-dom";
+import {Route, BrowserRouter as Router, Routes, Navigate} from "react-router-dom";
+import BookDetail from "./components/BookDetail.tsx";
+import NotFound from "./components/NotFound.tsx";
+import SecretBook from "./components/SecretBook.tsx";
+import {PrivateRoute} from "./components/PrivateRoute.tsx";
 
 function App() {
     return (
         <BooksProvider>
             <Router>
-                <Route path={"/"} element={<Books/>}></Route>
-                <h1>App</h1>
-                {/*<Route path={"/book"} element={<Navigate to={"/"}/>}></Route>*/}
+                <Routes>
+                    <Route path={"/"} element={<Books/>}></Route>
+                    <Route path={"/book"} element={<Navigate to={"/"}/>}></Route>
+                    <Route path={"/book/:bookId"} element={<BookDetail/>}></Route>
+                    <Route path={"*"} element={<NotFound/>}></Route>
+                    <Route path={"/secret"} element={<PrivateRoute component={<SecretBook/>} /> }></Route>
+                </Routes>
             </Router>
         </BooksProvider>
     )
--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,14 +1,23 @@
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
 import {Book, BooksContext} from "../BooksContext.tsx";
+import {useLocation} from "react-router-dom";
 
 export default function Books() {
 
     const { books } = useContext(BooksContext) || { books: [] };
+    const query = new URLSearchParams(useLocation().search);
+    const search = query.get("search") || "";
+    const filteredBooks = books.filter((book: Book) => book.title.toLowerCase().includes(search.toLowerCase()));
+
+    useEffect(() => {
+        console.log(query.get("search"));
+    }, []);
+
     return (
         <div>
             <h1>Books</h1>
             <ul>
-                {books.map((book: Book) => (
+                {filteredBooks.map((book: Book) => (
                     <li key={book.id}>{book.title}</li>
                 ))}
             </ul>
